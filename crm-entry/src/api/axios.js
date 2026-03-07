220e3cfc017340165f129b554654d26a76469b09
Suresh <Suresh@jetfyx.com>
2026-03-07 13:36:23 +0000

    Updated axios.js

--- crm-entry/src/api/axios.js
+++ crm-entry/src/api/axios.js
@@ -2,8 +2,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://89.116.20.215:9090/",
-  // baseURL: "https://crm.metagensoft.com/api/",
+  // baseURL: "http://89.116.20.215:9090/",
+  baseURL: "https://crmauth.metagensoft.com/api/",
   // baseURL: "https://albertine-nonempathic-heaven.ngrok-free.dev",
   // baseURL: "https://localhost:8080",
   headers: {
