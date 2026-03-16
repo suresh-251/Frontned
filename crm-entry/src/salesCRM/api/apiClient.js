2b9bf1eff1fbfcfff8bc3410472db0a940aaa8c6
Mythili Velivelli <mythili123@gmail.com>
2026-03-16 15:45:30 +0530

    Resolving prod issue

--- crm-entry/src/salesCRM/api/apiClient.js
+++ crm-entry/src/salesCRM/api/apiClient.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 const BASE_URL = window.location.hostname === "localhost"
   ? "http://89.116.20.215:9096/api"
-  : "http://89.116.20.215:9090/api";
+  : "https://crmsales.metagensoft.com/api";
 
 // Create Axios instance
 const apiClient = axios.create({
